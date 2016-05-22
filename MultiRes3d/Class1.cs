09aadfbe4e5f55923b1355d6029b7ca165524d7e
padedou <padedou@gmail.com>
2016-05-22 16:12:29 +0300

    Commit test

---
+++ MultiRes3d/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Test test test
+
+namespace MultiRes3d
+{
+    class Class1
+    {
+    }
+}
